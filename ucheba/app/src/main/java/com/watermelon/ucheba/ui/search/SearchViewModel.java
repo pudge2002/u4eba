ed25e2cfd7d16791a22a9911e055dc9836d27d2a
pudge2002 <silverhof634@gmail.com>
2024-09-15 18:53:02 +0300

    cringe

--- ucheba/app/src/main/java/com/watermelon/ucheba/ui/search/SearchViewModel.java
+++ ucheba/app/src/main/java/com/watermelon/ucheba/ui/search/SearchViewModel.java
@@ -1,16 +1,16 @@
-package com.watermelon.ucheba.ui.notifications;
+package com.watermelon.ucheba.ui.search;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class SearchViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public SearchViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("это аккаунт");
+        mText.setValue("это поиск");
     }
 
     public LiveData<String> getText() {
